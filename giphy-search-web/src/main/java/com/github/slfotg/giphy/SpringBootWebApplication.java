5b2b976805ea0d340843b761f56348f0c3892dfe
Sam Foster <slfotg@gmail.com>
2019-02-17 13:50:41 -0600

    Added spring security settings

--- giphy-search-web/src/main/java/com/github/slfotg/giphy/SpringBootWebApplication.java
+++ giphy-search-web/src/main/java/com/github/slfotg/giphy/SpringBootWebApplication.java
@@ -3,7 +3,9 @@ package com.github.slfotg.giphy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 @EnableAutoConfiguration
 public class SpringBootWebApplication {
---
+++ giphy-search-web/src/main/java/com/github/slfotg/giphy/security/SecurityConfiguration.java
@@ -0,0 +1,54 @@
+package com.github.slfotg.giphy.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.github.slfotg.giphy.service.GiphyUserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        // @formatter:off
+        httpSecurity.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/home").permitAll()
+                .antMatchers("/check").permitAll()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/webjars/**").permitAll()
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/js/**").permitAll()
+                .antMatchers("/api/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+        // @formatter:on
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(GiphyUserDetailsService userDetailsService) {
+        final DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(encoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(11);
+    }
+
+}
